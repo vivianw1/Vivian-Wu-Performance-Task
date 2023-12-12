634cdc687ba77fae38b68dd7e75ddc7cbada91d4
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2023-12-12 12:27:06 -0800

    World Scene Set Up

--- Vivian-Wu-Performance-Task/Player.gd
+++ Vivian-Wu-Performance-Task/Player.gd
@@ -5,16 +5,16 @@ extends Area2D
 
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_just_pressed("move right"):
+	if Input.is_action_pressed("move right"):
 		velocity.x += 1
-	if Input.is_action_just_pressed("move left"):
+	if Input.is_action_pressed("move left"):
 		velocity.x -= 1
-	if Input.is_action_just_pressed("move up"):
+	if Input.is_action_pressed("move up"):
 		velocity.y -= 1
-	if Input.is_action_just_pressed("move down"):
+	if Input.is_action_pressed("move down"):
 		velocity.y += 1
 	
-	position += velocity * delta
-
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
